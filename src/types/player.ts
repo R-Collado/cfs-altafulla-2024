716d2daafe48d07cfe37f356de19b110bb33b8fd
Roger Collado <roger.collado1130@gmail.com>
2024-08-16 12:35:00 +0200

    add player interface

---
+++ src/types/player.ts
@@ -0,0 +1,11 @@
+interface Player {
+  name: string;
+  surname: string;
+  nickname: string;
+  number: number;
+  img: string;
+  role: "keeper" | "defender" | "wing" | "pivot";
+  age: number;
+  birthdate: string;
+  birthplace: string;
+}
