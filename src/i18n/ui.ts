558413b234bc99a924744896a14ce7aa8d8b4fab
Roger Collado <roger.collado1130@gmail.com>
2024-08-19 17:05:06 +0200

    add internationalization functions

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -13,17 +13,20 @@ export const ui = {
     "nav.players": "Jugadors",
     "nav.sponsors": "Patrocinadors",
     "nav.contact": "Contacte",
+    "utils.close": "Tancar",
   },
   en: {
     "nav.news": "News",
     "nav.players": "Players",
     "nav.sponsors": "Sponsors",
     "nav.contact": "Contact",
+    "utils.close": "Close",
   },
   es: {
     "nav.news": "Noticias",
     "nav.players": "Jugadores",
     "nav.sponsors": "Patrocinadores",
     "nav.contact": "Contacto",
+    "utils.close": "Cerrar",
   },
 } as const;
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -5,6 +5,19 @@ export function getLangFromUrl(url: URL) {
   return defaultLanguage;
 }
 
+export function setLangToLocalStorage(lang: keyof typeof ui) {
+  localStorage.setItem("lang", lang);
+}
+
+export function getLangFromLocalStorage() {
+  const lang = localStorage.getItem("lang");
+  return lang === "es" ? "" : lang;
+}
+
+export function getLangFromLocation() {
+  return getLangFromUrl(new URL(window.location.href));
+}
+
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLanguage]) {
     return ui[lang][key] || ui[defaultLanguage][key];
