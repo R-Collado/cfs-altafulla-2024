9f1ca179fa41e8bec95c04df52bae8fa08e27f3a
Roger Collado <roger.collado1130@gmail.com>
2024-09-05 12:20:48 +0200

    update imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -6,4 +9,10 @@ export default defineConfig({
     defaultLocale: "es",
     locales: ["en", "es", "ca"],
   },
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
