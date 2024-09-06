0060ad0544e15095d58ca4ff81b91c98a98d10ba
Roger Collado <roger.collado1130@gmail.com>
2024-09-06 22:07:55 +0200

    idk

--- src/content/blog.ts
+++ src/content/blog.ts
@@ -2,7 +2,7 @@
 import { z, defineCollection } from "astro:content";
 
 // 2. Define a `type` and `schema` for each collection
-const blogCollection = defineCollection({
+export const blogCollection = defineCollection({
   type: "content", // v2.5.0 and later
   schema: ({ image }) =>
     z.object({
@@ -22,8 +22,8 @@ const blogCollection = defineCollection({
     }),
 });
 
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+// // // 3. Export a single `collections` object to register your collection(s)
+// // //    This key should match your collection directory name in "src/content"
 export const collections = {
   blog: blogCollection,
 };
--- src/content/players.ts
+++ src/content/players.ts
@@ -2,9 +2,9 @@
 import { z, defineCollection } from "astro:content";
 
 // 2. Define a `type` and `schema` for each collection
-const playersCollection = defineCollection({
+export const playersCollection = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: ({ image }) =>
+  schema: () =>
     z.object({
       name: z.string(),
       surname: z.string(),
@@ -12,8 +12,8 @@ const playersCollection = defineCollection({
     }),
 });
 
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+// // 3. Export a single `collections` object to register your collection(s)
+// // //    This key should match your collection directory name in "src/content"
 export const collections = {
   players: playersCollection,
 };
